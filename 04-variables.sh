cdf18368af05ffbbebb58a3ba9b75d20c62925ec
LohanU <uppala.lohan@gmail.com>
2024-08-26 17:05:48 +0530

    variable

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : hello $PERSON2 how are you"
+echo "$PERSON2 : Hi $PERSON1, I am fine. How are you doing?"
+echo "$PERSON1  : I am good $PERSON2, What's going on?"
+echo "$PERSON2  :  I started devops training"
