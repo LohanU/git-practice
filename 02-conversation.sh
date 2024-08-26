ad7240ec709063571e81fabd355b7a49473061b2
LohanU <uppala.lohan@gmail.com>
2024-08-26 16:01:58 +0530

    updated the conversation status

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : hello Suresh how are you"
+echo "Suresh : Hi Ramesh, I am fine. How are you doing?"
+echo "Ramesh  : I am good Suresh, What's going on?"
+echo "Suresh  :  I started devops training"
