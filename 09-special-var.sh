c56768aad5c5512803f0bd7d5f9863de6321638a
LohanU <uppala.lohan@gmail.com>
2024-08-27 14:02:08 +0530

    functions

--- 09-special-var.sh
+++ 09-special-var.sh
@@ -5,4 +5,7 @@ echo "all variables passed to the script: $@"
 echo "no.of variables passed : $#"
  
  echo "script name : $0"
- 
+
+ echo "present working directory : $PWD"
+
+ echo "Home directory of current user : $HOME"
